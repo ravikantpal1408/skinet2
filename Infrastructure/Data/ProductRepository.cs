24a1e27f5e9995bbe3c49b844227c4c06461662a
Ravi <ravi@Ravis-MacBook-Air.local>
2021-08-30 22:22:08 +0530

    done with and eager loading implementation

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -16,12 +16,17 @@ namespace Infrastructure.Data
         }
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FirstAsync(c => c.Id == id);
+            return await _context.Products
+                                        .Include(p => p.ProductType)
+                                        .Include(p => p.ProductBrand).FirstOrDefaultAsync(c => c.Id == id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                                        .Include(p => p.ProductType)
+                                        .Include(p => p.ProductBrand)
+                                        .ToListAsync();
         }
     }
 }
