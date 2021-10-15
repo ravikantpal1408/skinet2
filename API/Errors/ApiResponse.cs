99ba39d77a9d34fdec91846af7cd9d3f1220995c
ravikantpal1408 <ravikantpal123@gmail.com>
2021-10-16 00:37:26 +0530

    done with configuraion of missing end point handler

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A Bad Request, You made",
+                401 => "Authorize, you are not allowed",
+                404 => "Resource not found",
+                500 => "Internal Server Error. Due to some internal crashing we are unable to process your request",
+                _ => null
+            };
+        }
+
+    }
+}
